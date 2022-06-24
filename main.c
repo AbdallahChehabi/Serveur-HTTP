871e939fd5d0c989cc8715a0491f5e2fe1b0005c
CHEHABI <46402196+AbdallahChehabi@users.noreply.github.com>
2022-06-24 22:25:39 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "server.h"
+
+int main(int c, char** v)
+{
+    serve_forever("12913");
+    return 0;
+}
+
+void route()
+{
+    ROUTE_START()
+
+    ROUTE_GET("/")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Bonjour!\n");
+        printf("You are using %s", request_header("User-Agent"));
+    }
+
+    ROUTE_POST("/")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Wow, seems that you POSTed %d bytes. \r\n", payload_size);
+        printf("Fetch the data using `payload` variable.");
+    }
+  
+    ROUTE_END()
+}
